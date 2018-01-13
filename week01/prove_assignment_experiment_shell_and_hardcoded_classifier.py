2ee8c853029820b4a743c00619af8bef0ead1db3
Mr Pig <zman739@hotmail.com>
2018-01-13 12:04:25 -0700

    finish step 3

--- week01/prove_assignment_experiment_shell_and_hardcoded_classifier.py
+++ week01/prove_assignment_experiment_shell_and_hardcoded_classifier.py
@@ -1,5 +1,6 @@
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
 
 iris = datasets.load_iris()
 
@@ -12,4 +13,8 @@ print(iris.target)
 # Show the actual target names that correspond to each number
 print(iris.target_names)
 
-train_test_split(iris.data, test_size=0.3, train_size=0.7)
+data_train, data_test, targets_train, targets_test = train_test_split(iris.data, iris.target, test_size=0.3, train_size=0.7)
+
+classifier = GaussianNB()
+
+model = classifier.fit(data_train, targets_train)

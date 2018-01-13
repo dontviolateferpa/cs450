09a1f332a1d824642da494dbae53ea2ecc37ce2d
ijustmadeanaccount <zman739@hotmail.com>
2018-01-13 15:18:03 -0700

    AAB - load from CSV

--- week01/prove_assignment_experiment_shell_and_hardcoded_classifier.py
+++ week01/prove_assignment_experiment_shell_and_hardcoded_classifier.py
@@ -1,3 +1,5 @@
+import numpy as np
+from numpy import genfromtxt
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
@@ -25,13 +27,24 @@ class HardCodedClassifier:
 iris = datasets.load_iris()
 
 # Show the data (the attributes of each instance)
+print "Iris Data"
 print(iris.data)
+print ""
+np.savetxt("iris-data.csv", iris.data, delimiter=",")
+iris_csv_data = genfromtxt('iris-data.csv', delimiter=',')
+print "Iris CSV data"
+print(iris_csv_data)
+print ""
 
 # Show the target values (in numeric format) of each instance
+print "Iris Target"
 print(iris.target)
+print ""
 
 # Show the actual target names that correspond to each number
+print "Iris Target Names"
 print(iris.target_names)
+print ""
 
 data_train, data_test, targets_train, targets_test = train_test_split(iris.data, iris.target, test_size=0.3, train_size=0.7)
 

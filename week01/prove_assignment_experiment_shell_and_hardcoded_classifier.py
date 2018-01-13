fd0d71fb2343bf61cb3606d4d1674bc5827ad1d8
ijustmadeanaccount <zman739@hotmail.com>
2018-01-12 21:43:37 -0700

    finish step 2

--- week01/prove_assignment_experiment_shell_and_hardcoded_classifier.py
+++ week01/prove_assignment_experiment_shell_and_hardcoded_classifier.py
@@ -1,1 +1,15 @@
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
 
+iris = datasets.load_iris()
+
+# Show the data (the attributes of each instance)
+print(iris.data)
+
+# Show the target values (in numeric format) of each instance
+print(iris.target)
+
+# Show the actual target names that correspond to each number
+print(iris.target_names)
+
+train_test_split(iris.data, test_size=0.3, train_size=0.7)

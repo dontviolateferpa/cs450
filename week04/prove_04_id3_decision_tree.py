8dd2514fdf5da5bc3d9871af31c0acea360ddcdd
ijustmadeanaccount <zman739@hotmail.com>
2018-02-03 14:03:41 -0700

    move entropy calculation to model class

--- week04/prove_04_id3_decision_tree.py
+++ week04/prove_04_id3_decision_tree.py
@@ -30,16 +30,67 @@ class DTCModel:
 
         self._make_ID3_tree()
 
-    def calc_entropy():
-        """calculate the entropy of a given class"""
-        pass
+    def _calc_entropy(self, numers, denom):
+        """calc the entropy for this particular attribute's value"""
+        total_entropy = 0
+        i = 0
+        for numer in numers:
+            p = numers[i] / float(denom)
+            total_entropy = total_entropy - (p * np.log2(p))
+            i = i + 1
+        return total_entropy
 
-    def _make_ID3_tree():
+    def _calc_entropies_aux(self, array_dicts):
+        """calculate the entropy of each attribute, helper function"""
+        entropies = []
+        for i_dict in array_dicts:
+            numers = []
+            weight = 0
+            sum_weight = 0
+            sum_entropy = 0
+            for key in i_dict:
+                # this will be needed to compute the average entropy between values of attributes
+                weight = len(i_dict[key])
+                for key, value in Counter(i_dict[key]).iteritems():
+                    numers.append(value)
+                # make a sum of total entropy
+                sum_entropy = sum_entropy + self._calc_entropy(numers, weight) * weight
+                sum_weight = sum_weight + weight
+                numers = []
+            entropy = sum_entropy / float(sum_weight)
+            # make a list of our entropies
+            entropies.append(entropy)
+
+        return entropies
+
+    def _calc_entropies(self, train_data, train_target):
+        """calculate the entropy of each attribute"""
+        buckets = []
+        cols = []
+        cidx = 0
+        for col in train_data.columns:
+            # store the column index in along with the column name
+            buckets.append({})
+            cols.append(col)
+            for ridx, row in train_data[col].iteritems():
+                if row in buckets[cidx]:
+                    buckets[cidx][row].append(train_target[ridx])
+                else:
+                    buckets[cidx][row] = []
+                    buckets[cidx][row].append(train_target[ridx])
+            cidx = cidx + 1
+        # turn this into a dictionary of column names with their values as the entropies
+        entropies_dict = {}
+        entropies = self._calc_entropies_aux(buckets)
+        e_count = 0
+        for entropy in entropies:
+            entropies_dict[cols[e_count]] = entropy
+            e_count = e_count + 1
+        return entropies_dict
+
+    def _make_ID3_tree(self):
         """make the ID3 decision tree"""
-        col_entropies = []
-        # iterate through each column in the df
-        for col in self._train_target.columns:
-            print self._train_target[col]
+        print self._calc_entropies(self._train_data, self._train_target)
 
     def predict(self, test_data):
         """"""
@@ -90,69 +141,12 @@ def prep_data(args):
 
     return df, None, df_target, None
 
-def calc_entropy(numers, denom):
-    """calc the entropy for this particular attribute's value"""
-    total_entropy = 0
-    i = 0
-    for numer in numers:
-        p = numers[i] / float(denom)
-        total_entropy = total_entropy - (p * np.log2(p))
-        i = i + 1
-    return total_entropy
-
-def calc_entropies_aux(array_dicts):
-    """calculate the entropy of each attribute, helper function"""
-    entropies = []
-    for i_dict in array_dicts:
-        numers = []
-        weight = 0
-        sum_weight = 0
-        sum_entropy = 0
-        for key in i_dict:
-            # this will be needed to compute the average entropy between values of attributes
-            weight = len(i_dict[key])
-            for key, value in Counter(i_dict[key]).iteritems():
-                numers.append(value)
-            # make a sum of total entropy
-            sum_entropy = sum_entropy + calc_entropy(numers, weight) * weight
-            sum_weight = sum_weight + weight
-            numers = []
-        entropy = sum_entropy / float(sum_weight)
-        # make a list of our entropies
-        entropies.append(entropy)
-
-    return entropies
-
-def calc_entropies(train_data, train_target):
-    """calculate the entropy of each attribute"""
-    buckets = []
-    cols = []
-    cidx = 0
-    for col in train_data.columns:
-        # store the column index in along with the column name
-        buckets.append({})
-        cols.append(col)
-        for ridx, row in train_data[col].iteritems():
-            if row in buckets[cidx]:
-                buckets[cidx][row].append(train_target[ridx])
-            else:
-                buckets[cidx][row] = []
-                buckets[cidx][row].append(train_target[ridx])
-        cidx = cidx + 1
-    # turn this into a dictionary of column names with their values as the entropies
-    entropies_dict = {}
-    entropies = calc_entropies_aux(buckets)
-    e_count = 0
-    for entropy in entropies:
-        entropies_dict[cols[e_count]] = entropy
-        e_count = e_count + 1
-    return entropies_dict
-
 def main():
     """everything happens here"""
     args = receive_args().parse_args()
     train_data, test_data, train_target, test_target = prep_data(args)
+    model = MyDecisionTreeClassifier().fit(train_data, train_target)
 
-    print calc_entropies(train_data, train_target)
+    # print calc_entropies(train_data, train_target)
 
 main()

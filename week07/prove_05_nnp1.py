69ad2d93b7fbc689c99ea3b3e7e4636550cfa7b7
Mr Pig <zman739@hotmail.com>
2018-02-18 23:17:42 -0700

    create prove_05

---
+++ week07/prove_05_nnp1.py
@@ -0,0 +1,7 @@
+"""
+Notice that at this point, we are NOT implementing any sort of training or learning
+in our algorithm. The point of this part of the assignment is to make sure you have
+the basics of a network of nodes in place that can handle a real instance from a
+dataset.
+"""
+

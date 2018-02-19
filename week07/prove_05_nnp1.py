d689bcefd6f3f1c18c46dd5fd2a747d88a482f7f
Mr Pig <zman739@hotmail.com>
2018-02-18 23:45:55 -0700

    receive args from user

--- week07/prove_05_nnp1.py
+++ week07/prove_05_nnp1.py
@@ -5,3 +5,29 @@ the basics of a network of nodes in place that can handle a real instance from a
 dataset.
 """
 
+import argparse
+import numpy as np
+
+class Network:
+    """the Network contains nodes"""
+
+    def __init__(self):
+        """initialize the class"""
+        pass
+
+def receive_args():
+    """receive arguments from the user pass to the script"""
+    parser = argparse.ArgumentParser(description='Pass arguments to the script')
+    parser.add_argument('--csv_file',
+                        dest='csv_file',
+                        action='store',
+                        choices=["iris.csv", "pima.csv"],
+                        required=True)
+
+    return parser
+
+def main():
+    """where the magic happens"""
+    args = receive_args().parse_args()
+
+main()

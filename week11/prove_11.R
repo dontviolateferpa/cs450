fe9e69bd60470e79e82b9299454200e659418883
ijustmadeanaccount <zman739@hotmail.com>
2018-03-20 22:14:49 -0600

    no... not doing this in R

---
+++ week11/prove_11.R
@@ -0,0 +1,7 @@
+# prove 11
+
+df_chss <- read.csv("C:\\Users\\Zachary\\OneDrive\\Documents\\09 BYUI 2018 Winter (Override)\\CS 450 - Machine Learning and Data Mining\\cs450\\week11\\chess.csv", header=TRUE)
+df_iris <- read.csv("C:\\Users\\Zachary\\OneDrive\\Documents\\09 BYUI 2018 Winter (Override)\\CS 450 - Machine Learning and Data Mining\\cs450\\week11\\iris.csv", header=TRUE)
+df_lttr <- read.csv("C:\\Users\\Zachary\\OneDrive\\Documents\\09 BYUI 2018 Winter (Override)\\CS 450 - Machine Learning and Data Mining\\cs450\\week11\\letters.csv", header=TRUE)
+
+# df_chss_tar
---
+++ week11/prove_11.py
@@ -0,0 +1,63 @@
+import sklearn
+import numpy as np
+import pandas as pd
+
+COLS_CHESS  = ["wk_file", "wk_rank", "wr_file", "wr_rank", "bk_file", "bk_rank", "depth_of_win"]
+COLS_IRIS   = ["sepal_len", "sepal_wid", "petal_len", "petal_wid", "class"]
+COLS_LETTER = ["letter", "x-box", "y-box", "wid-wid", "high-height", "onpix", "x-bar", "y-bar",
+               "x2bar", "y2bar", "xybar", "x2ybr", "xy2br", "x-ege", "xegvy", "y-ege", "yegvx"]
+
+def get_chess_dataset_categorical():
+    pass
+
+def get_letter_dataset_categorical():
+    pass
+
+def get_iris_dataset_categorical():
+    pass
+
+def get_chess_dataset_numerical():
+    pass
+
+def get_letter_dataset_numerical():
+    pass
+
+def get_iris_dataset_numerical():
+    pass
+
+def main():
+    """magic happens here"""
+    # preprocessing
+
+    # For each dataset
+
+    ## Try at least 3 different "regular" learning algorithms and note the results.
+    ### DS1
+    ##### method 1
+    ##### method 2
+    ##### method 3
+    ### DS2
+    ##### method 1
+    ##### method 2
+    ##### method 3
+    ### DS3
+    ##### method 1
+    ##### method 2
+    ##### method 3
+
+    ## Use Bagging and note the results. (Play around with a few different options)
+    ### DS1
+    ### DS2
+    ### DS3
+
+    ## Use AdaBoost and note the results. (Play around with a few different options)
+    ### DS1
+    ### DS2
+    ### DS3
+
+    ## Use a random forest and note the results. (Play around with a few different options)
+    ### DS1
+    ### DS2
+    ### DS3
+
+main()

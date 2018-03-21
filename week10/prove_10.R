428fa82ba49a216cef46d835c5484a90b1b691f2
ijustmadeanaccount <zman739@hotmail.com>
2018-03-20 21:02:47 -0600

    I don't remember what I did

--- week10/prove_10.R
+++ week10/prove_10.R
@@ -69,8 +69,9 @@ summary(hc4)
 ### USING K-MEANS ###
 
 
+# 01 - 03
 ## Cluster into k=5 clusters:
-myClusters = kmeans(data_scaled, 5)
+myClusters = kmeans(data_scaled, 3)
 
 ## Summary of the clusters
 summary(myClusters)
@@ -85,7 +86,66 @@ myClusters$cluster
 myClusters$withinss
 myClusters$tot.withinss
 
-
 ## Plotting a visual representation of k-means clusters
 library(cluster)
 clusplot(data_scaled, myClusters$cluster, color=TRUE, shade=TRUE, labels=2, lines=0)
+
+error = NULL
+for (i in 1:25) {
+  tempClusters = kmeans(data_scaled, i)
+  error[i] = tempClusters$tot.withinss
+}
+plot(error)
+
+# 04 - Evaluate the plot from the previous item, and choose an appropriate k-value using
+#      the "elbow method" mentioned in your reading. Then re-cluster a single time using
+#      that k-value. Use this clustering for the remaining questions.
+
+## Cluster into k=5 clusters:
+myClusters_elbow = kmeans(data_scaled, 5)
+
+## Summary of the clusters
+summary(myClusters_elbow)
+
+## Centers (mean values) of the clusters
+myClusters_elbow$centers
+
+## Within-cluster sum of squares and total sum of squares across clusters
+myClusters_elbow$withinss
+myClusters_elbow$tot.withinss
+
+## Plotting a visual representation of k-means clusters
+clusplot(data_scaled, myClusters_elbow$cluster, color=TRUE, shade=TRUE, labels=2, lines=0)
+
+## Cluster assignments (list the state in each cluster)
+myClusters_elbow$cluster
+
+
+
+### ABOVE AND BEYOND ###
+dataVolcano = volcano
+summary(dataVolcano)
+
+## scale the data
+data_scaled_volc = scale(dataVolcano)
+
+## compute a distance matrix
+distance_volc = dist(as.matrix(data_scaled_volc))
+
+## perform the clustering
+hc_volc = hclust(distance_volc)
+
+## plot the dendogram
+plot(hc_volc)
+
+
+error_volc = NULL
+for (i in 1:25) {
+  tempClusters = kmeans(data_scaled_volc, i)
+  error_volc[i] = tempClusters$tot.withinss
+}
+plot(error_volc)
+
+myClustersElbowVolc = kmeans(data_scaled, 4)
+
+clusplot(data_scaled_volc, myClustersElbowVolc$cluster, color=TRUE, shade=TRUE, labels=2, lines=0)

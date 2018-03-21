5b4101e33dc5e9ef3a8e48aed78faf55b935aa85
ijustmadeanaccount <zman739@hotmail.com>
2018-03-20 23:41:13 -0600

    update display_similarity function

--- week11/prove_11.py
+++ week11/prove_11.py
@@ -1,5 +1,6 @@
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
+from sklearn.tree import DecisionTreeClassifier
 import numpy as np
 import pandas as pd
 import difflib
@@ -8,6 +9,8 @@ COLS_CHESS  = ["wk_file", "wk_rank", "wr_file", "wr_rank", "bk_file", "bk_rank",
 COLS_IRIS   = ["sepal_len", "sepal_wid", "petal_len", "petal_wid", "class"]
 COLS_LETTER = ["letter", "x-box", "y-box", "wid-wid", "high-height", "onpix", "x-bar", "y-bar",
                "x2bar", "y2bar", "xybar", "x2ybr", "xy2br", "x-ege", "xegvy", "y-ege", "yegvx"]
+
+np.set_printoptions(threshold=20)
 
 def read_csv(cols, filename):
     return pd.io.parsers.read_csv(
@@ -67,16 +70,14 @@ def tts_chess_categorical():
     chess_cat_d, chess_cat_t= get_chess_dataset_categorical()
     return train_test_split(chess_cat_d, chess_cat_t, test_size=0.3, random_state=3)
 
-def display_similarity(predictions, targets_test, method):
-    """display similarity between predictions and test targets"""
-    if isinstance(predictions, pd.DataFrame) or isinstance(predictions, pd.Series):
-        predictions = predictions.as_matrix()
-    if isinstance(targets_test, pd.DataFrame) or isinstance(targets_test, pd.Series):
-        targets_test = targets_test.as_matrix()
-    print(predictions)
-    print(targets_test)
-    sm=difflib.SequenceMatcher(None, predictions, targets_test)
-    print("The two are " + str(sm.ratio()) + " percent similar (" + method + ")")
+def display_similarity(p, t, method):
+    """display similarity between predictions (p) and test targets (t)"""
+    if isinstance(p, pd.DataFrame) or isinstance(p, pd.Series):
+        p = p.as_matrix()
+    if isinstance(t, pd.DataFrame) or isinstance(t, pd.Series):
+        t = t.as_matrix()
+    error = np.mean(p != t)
+    print("The two are " + str(1.0 - error) + " percent similar (" + method + ")")
 
 def main():
     """magic happens here"""
@@ -85,17 +86,22 @@ def main():
     iris_cat_d = get_iris_dataset_categorical()
 
     chess_num_datatrain, chess_num_datatest, chess_num_targettrain, chess_num_targettest = tts_chess_numeric()
+    chess_cat_datatrain, chess_cat_datatest, chess_cat_targettrain, chess_cat_targettest = tts_chess_categorical()
     # For each dataset
 
     ## Try at least 3 different "regular" learning algorithms and note the results.
     ### DS1 - chess
     ##### method 1 - MLP **
-    clf_chess_num = MLPClassifier(solver='lbfgs', alpha=1e-5,
-                                  hidden_layer_sizes=(5,3), random_state=1)
-    clf_chess_num.fit(chess_num_datatrain, chess_num_targettrain)
-    predictions = clf_chess_num.predict(chess_num_datatest)
+    clf_chess_num_MLP = MLPClassifier(solver='lbfgs', alpha=1e-5,
+                                  hidden_layer_sizes=(40,30), random_state=1)
+    clf_chess_num_MLP.fit(chess_num_datatrain, chess_num_targettrain)
+    predictions = clf_chess_num_MLP.predict(chess_num_datatest)
     display_similarity(predictions, chess_num_targettest, "Neural Network on Chess Dataset")
-    ##### method 2
+    ##### method 2 - Decision Tree
+    clf_chess_num_DT = DecisionTreeClassifier(random_state=0)
+    clf_chess_num_DT.fit(chess_num_datatrain, chess_num_targettrain)
+    predictions = clf_chess_num_DT.predict(chess_num_datatest)
+    display_similarity(predictions, chess_num_targettest, "Decision Tree on Chess Dataset")
     ##### method 3
     ### DS2
     ##### method 1

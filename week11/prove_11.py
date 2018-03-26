a980bcea93d83e4370ba6d3e131259631972b8f0
ijustmadeanaccount <zman739@hotmail.com>
2018-03-25 20:14:53 -0600

    finish bagging

--- week11/prove_11.py
+++ week11/prove_11.py
@@ -3,6 +3,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import BaggingClassifier
 import numpy as np
 import pandas as pd
 import difflib
@@ -120,93 +121,105 @@ def main():
     iris_num_datatrain, iris_num_datatest, iris_num_targettrain, iris_num_targettest = tts_iris_numeric()
     letter_num_datatrain, letter_num_datatest, letter_num_targettrain, letter_num_targettest = tts_letter_numeric()
     
-    # For each dataset
-    ## Try at least 3 different "regular" learning algorithms and note the results.
-    ### DS1 - chess
-    print("")
-    ##### method 1 - MLP **
-    clf_chess_num_MLP = MLPClassifier(solver='adam', alpha=1e-5,
-                                      hidden_layer_sizes=(40,30), random_state=1)
-    clf_chess_num_MLP.fit(chess_num_datatrain, chess_num_targettrain)
-    predictions = clf_chess_num_MLP.predict(chess_num_datatest)
-    display_similarity(predictions, chess_num_targettest, "Chess - Neural Network")
-    ##### method 2 - Decision Tree
-    clf_chess_num_DT = DecisionTreeClassifier(random_state=0)
-    clf_chess_num_DT.fit(chess_num_datatrain, chess_num_targettrain)
-    predictions = clf_chess_num_DT.predict(chess_num_datatest)
-    display_similarity(predictions, chess_num_targettest, "Chess - Decision Tree")
-    ##### method 3 - KNN
-    clf_chess_num_KNN = KNeighborsClassifier(n_neighbors=7)
-    clf_chess_num_KNN.fit(chess_num_datatrain, chess_num_targettrain)
-    predictions = clf_chess_num_KNN.predict(chess_num_datatest)
-    display_similarity(predictions, chess_num_targettest, "Chess - KNN")
-    ### DS2 - iris
-    print("")
-    ##### method 1 - MLP
-    clf_iris_num_MLP = MLPClassifier(solver='adam', alpha=1e-5,
-                                     hidden_layer_sizes=(10,7), random_state=1)
-    clf_iris_num_MLP.fit(iris_num_datatrain, iris_num_targettrain)
-    predictions = clf_iris_num_MLP.predict(iris_num_datatest)
-    display_similarity(predictions, iris_num_targettest, "Iris - Neural Network")
-    # clf_iris_num_MLP_gs = MLPClassifier()
-    # iris_param_grid = [
-    #     {
-    #         'activation' : ['identity', 'logistic', 'tanh', 'relu'],
-    #         'solver' : ['lbfgs', 'sgd', 'adam'],
-    #         'hidden_layer_sizes': [
-    #          (9,1),(9,2),(9,3),(9,4),(9,5),(9,6),(9,7),(9,8),(9,10),(9,11),(9,12),
-    #          (10,1),(10,2),(10,3),(10,4),(10,5),(10,6),(10,7),(10,8),(10,10),(10,11),(10,12),
-    #          (11,1),(11,2),(11,3),(11,4),(11,5),(11,6),(11,7),(11,8),(11,10),(11,11),(11,12)
-    #          ]
-    #     }
-    #    ]
-    # grid_clf = GridSearchCV(clf_iris_num_MLP_gs, iris_param_grid, cv=3,
-    #                        scoring='accuracy')
-    # grid_clf.fit(iris_num_datatrain, iris_num_targettrain)
-    # print("the best parameters out of those chosen are: ")
-    # print(grid_clf.best_params_)
-    ##### method 2 - Decision Tree
-    clf_iris_num_DT = DecisionTreeClassifier()
-    clf_iris_num_DT.fit(iris_num_datatrain, iris_num_targettrain)
-    predictions = clf_iris_num_DT.predict(iris_num_datatest)
-    display_similarity(predictions, iris_num_targettest, "Iris - Decision Tree")
-    ##### method 3 - KNN
-    clf_iris_num_KNN = KNeighborsClassifier(n_neighbors=3)
-    clf_iris_num_KNN.fit(iris_num_datatrain, iris_num_targettrain)
-    predictions = clf_iris_num_KNN.predict(iris_num_datatest)
-    display_similarity(predictions, iris_num_targettest, "Iris - KNN")
-    ### DS3
-    print("")
-    ##### method 1 - MLP
-    clf_letter_num_MLP = MLPClassifier(solver='adam', alpha=1e-5,
-                                      hidden_layer_sizes=(40,30), random_state=1)
-    clf_letter_num_MLP.fit(letter_num_datatrain, letter_num_targettrain)
-    predictions = clf_letter_num_MLP.predict(letter_num_datatest)
-    display_similarity(predictions, letter_num_targettest, "Letter - Neural Network")
-    ##### method 2 - Decision Tree
-    clf_letter_num_DT = DecisionTreeClassifier()
-    clf_letter_num_DT.fit(letter_num_datatrain, letter_num_targettrain)
-    predictions = clf_letter_num_DT.predict(letter_num_datatest)
-    display_similarity(predictions, letter_num_targettest, "Letter - Decision Tree")
-    ##### method 3 - KNN
-    clf_letter_num_KNN = KNeighborsClassifier(n_neighbors=3)
-    clf_letter_num_KNN.fit(letter_num_datatrain, letter_num_targettrain)
-    predictions = clf_letter_num_KNN.predict(letter_num_datatest)
-    display_similarity(predictions, letter_num_targettest, "Letter - KNN")
+    # # For each dataset
+    # ## Try at least 3 different "regular" learning algorithms and note the results.
+    # ### DS1 - chess
+    # print("")
+    # ##### method 1 - MLP **
+    # clf_chess_num_MLP = MLPClassifier(solver='adam', alpha=1e-5,
+    #                                   hidden_layer_sizes=(40,30), random_state=1)
+    # clf_chess_num_MLP.fit(chess_num_datatrain, chess_num_targettrain)
+    # predictions = clf_chess_num_MLP.predict(chess_num_datatest)
+    # display_similarity(predictions, chess_num_targettest, "Chess - Neural Network")
+    # ##### method 2 - Decision Tree
+    # clf_chess_num_DT = DecisionTreeClassifier(random_state=0)
+    # clf_chess_num_DT.fit(chess_num_datatrain, chess_num_targettrain)
+    # predictions = clf_chess_num_DT.predict(chess_num_datatest)
+    # display_similarity(predictions, chess_num_targettest, "Chess - Decision Tree")
+    # ##### method 3 - KNN
+    # clf_chess_num_KNN = KNeighborsClassifier(n_neighbors=7)
+    # clf_chess_num_KNN.fit(chess_num_datatrain, chess_num_targettrain)
+    # predictions = clf_chess_num_KNN.predict(chess_num_datatest)
+    # display_similarity(predictions, chess_num_targettest, "Chess - KNN")
+    # ### DS2 - iris
+    # print("")
+    # ##### method 1 - MLP
+    # clf_iris_num_MLP = MLPClassifier(solver='adam', alpha=1e-5,
+    #                                  hidden_layer_sizes=(10,7), random_state=1)
+    # clf_iris_num_MLP.fit(iris_num_datatrain, iris_num_targettrain)
+    # predictions = clf_iris_num_MLP.predict(iris_num_datatest)
+    # display_similarity(predictions, iris_num_targettest, "Iris - Neural Network")
+    # # clf_iris_num_MLP_gs = MLPClassifier()
+    # # iris_param_grid = [
+    # #     {
+    # #         'activation' : ['identity', 'logistic', 'tanh', 'relu'],
+    # #         'solver' : ['lbfgs', 'sgd', 'adam'],
+    # #         'hidden_layer_sizes': [
+    # #          (9,1),(9,2),(9,3),(9,4),(9,5),(9,6),(9,7),(9,8),(9,10),(9,11),(9,12),
+    # #          (10,1),(10,2),(10,3),(10,4),(10,5),(10,6),(10,7),(10,8),(10,10),(10,11),(10,12),
+    # #          (11,1),(11,2),(11,3),(11,4),(11,5),(11,6),(11,7),(11,8),(11,10),(11,11),(11,12)
+    # #          ]
+    # #     }
+    # #    ]
+    # # grid_clf = GridSearchCV(clf_iris_num_MLP_gs, iris_param_grid, cv=3,
+    # #                        scoring='accuracy')
+    # # grid_clf.fit(iris_num_datatrain, iris_num_targettrain)
+    # # print("the best parameters out of those chosen are: ")
+    # # print(grid_clf.best_params_)
+    # ##### method 2 - Decision Tree
+    # clf_iris_num_DT = DecisionTreeClassifier()
+    # clf_iris_num_DT.fit(iris_num_datatrain, iris_num_targettrain)
+    # predictions = clf_iris_num_DT.predict(iris_num_datatest)
+    # display_similarity(predictions, iris_num_targettest, "Iris - Decision Tree")
+    # ##### method 3 - KNN
+    # clf_iris_num_KNN = KNeighborsClassifier(n_neighbors=3)
+    # clf_iris_num_KNN.fit(iris_num_datatrain, iris_num_targettrain)
+    # predictions = clf_iris_num_KNN.predict(iris_num_datatest)
+    # display_similarity(predictions, iris_num_targettest, "Iris - KNN")
+    # ### DS3
+    # print("")
+    # ##### method 1 - MLP
+    # clf_letter_num_MLP = MLPClassifier(solver='adam', alpha=1e-5,
+    #                                   hidden_layer_sizes=(40,30), random_state=1)
+    # clf_letter_num_MLP.fit(letter_num_datatrain, letter_num_targettrain)
+    # predictions = clf_letter_num_MLP.predict(letter_num_datatest)
+    # display_similarity(predictions, letter_num_targettest, "Letter - Neural Network")
+    # ##### method 2 - Decision Tree
+    # clf_letter_num_DT = DecisionTreeClassifier()
+    # clf_letter_num_DT.fit(letter_num_datatrain, letter_num_targettrain)
+    # predictions = clf_letter_num_DT.predict(letter_num_datatest)
+    # display_similarity(predictions, letter_num_targettest, "Letter - Decision Tree")
+    # ##### method 3 - KNN
+    # clf_letter_num_KNN = KNeighborsClassifier(n_neighbors=3)
+    # clf_letter_num_KNN.fit(letter_num_datatrain, letter_num_targettrain)
+    # predictions = clf_letter_num_KNN.predict(letter_num_datatest)
+    # display_similarity(predictions, letter_num_targettest, "Letter - KNN")
 
     ## Use Bagging and note the results. (Play around with a few different options)
-    ### DS1
-    ### DS2
-    ### DS3
+    ### DS1 - Chess
+    clf_chess_num_Bagging = BaggingClassifier(bootstrap=True, n_estimators=20)
+    clf_chess_num_Bagging.fit(chess_num_datatrain, chess_num_targettrain)
+    predictions = clf_chess_num_Bagging.predict(chess_num_datatest)
+    display_similarity(predictions, chess_num_targettest, "BAGGING - Chess")
+    ### DS2 - Iris
+    clf_iris_num_Bagging = BaggingClassifier(bootstrap=True)
+    clf_iris_num_Bagging.fit(iris_num_datatrain, iris_num_targettrain)
+    predictions = clf_iris_num_Bagging.predict(iris_num_datatest)
+    display_similarity(predictions, iris_num_targettest, "BAGGING - Iris")
+    ### DS3 - Letter
+    clf_letter_num_Bagging = BaggingClassifier(bootstrap=False)
+    clf_letter_num_Bagging.fit(letter_num_datatrain, letter_num_targettrain)
+    predictions = clf_letter_num_Bagging.predict(letter_num_datatest)
+    display_similarity(predictions, letter_num_targettest, "BAGGING - Letter")
 
     ## Use AdaBoost and note the results. (Play around with a few different options)
-    ### DS1
-    ### DS2
-    ### DS3
+    ### DS1 - Chess
+    ### DS2 - Iris
+    ### DS3 - Letter
 
     ## Use a random forest and note the results. (Play around with a few different options)
-    ### DS1
-    ### DS2
-    ### DS3
+    ### DS1 - Chess
+    ### DS2 - Iris
+    ### DS3 - Letter
 
 main()
